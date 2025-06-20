a49d677743eb3365986c2582c98accc35758d5a0
Nguyễn Hữu Vinh <huuvinh63@gmail.com>
2025-06-20 17:39:34 +0700

    Delete 100year.py

--- 100year.py
+++
@@ -1,7 +0,0 @@
-Name = str(input('Nhập tên của bạn:'))
-Age = int(input('Nhập tuổi của bạn:'))
-import datetime
-yearnow = datetime.datetime.now().year
-a = yearnow + 100 - Age
-print(f'Đến năm {a}, bạn {Name} sẽ tròn 100 tuổi')
-input('Press Enter to Exit...')
